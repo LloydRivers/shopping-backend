58e56232d12a010379f7ec2135c55014cba33dbc
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2024-03-25 21:26:39 +0000

    reverted imports

--- src/routes/getAllProducts.ts
+++ src/routes/getAllProducts.ts
@@ -1,5 +1,4 @@
 import { Context } from 'koa';
-import { ProductsService } from '../modules/products/services/concrete/ProductsService';
 import { ProductsController } from '../modules/products/adaptors/controllers/Product/ProductsController';
 import { container } from '../config/inversify.config';
 import { TYPES } from '../config/types';
