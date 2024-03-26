14d52b4327cd008cd15a7e83196253f4f8fa18f6
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2024-03-26 21:07:25 +0000

    refactor to account for "this" binding

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -3,19 +3,24 @@ import { ProductsController } from '../modules/products/adaptors/controllers/Pro
 import { container } from '../config/inversify.config';
 import { TYPES } from '../config/types';
 import { IProductDTO } from '@modules/products/DTO/ProductDTO';
-export const getAllProducts = async (ctx: Context) => {
+
+function productContainer() {
   const productsController = container.get<ProductsController>(TYPES.ProductsController);
+  return productsController;
+}
+export async function getAllProducts(ctx: Context) {
+  const productsController = productContainer();
   ctx.body = await productsController.getAllProducts();
-};
+}
 
-export const getProductById = async (ctx: Context) => {
+export async function getProductById(ctx: Context) {
   const productId = ctx.params.id;
-  const productsController = container.get<ProductsController>(TYPES.ProductsController);
+  const productsController = productContainer();
   ctx.body = await productsController.getProductByID(productId);
-};
+}
 
-export const postProduct = async (ctx: Context) => {
-  const productsController = container.get<ProductsController>(TYPES.ProductsController);
+export async function postProduct(ctx: Context) {
+  const productsController = productContainer();
   const requestBody: IProductDTO = ctx.request.body as IProductDTO;
   ctx.body = await productsController.postProduct(requestBody);
-};
+}
