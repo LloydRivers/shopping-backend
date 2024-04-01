fb7e5d4fbb97aed111e4ea82448fe1d32506403d
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2024-04-01 09:32:50 +0100

    dotenv (Check later)
    I thought this was globally available, but the linter is throwing errors.
    Mini spike needed.

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import bodyParser from 'koa-bodyparser';
 import cors from '@koa/cors';
 import productRoutes from './routes/productRoutes';
 import documentationRoutes from './routes/documentationRoutes';
-require('dotenv').config();
+import dotenv from 'dotenv';
+dotenv.config();
 
 const main = (async () => {
   const app = new Koa();
