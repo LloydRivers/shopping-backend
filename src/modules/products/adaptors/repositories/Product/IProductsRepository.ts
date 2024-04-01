ea6608455268acb459638c941cc86d5410d49037
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2024-04-01 09:32:05 +0100

    Testing out the types generated by prisma
    Huge refactor incoming if this works

--- src/modules/products/adaptors/repositories/Product/IProductsRepository.ts
+++ src/modules/products/adaptors/repositories/Product/IProductsRepository.ts
@@ -1,5 +1,7 @@
 import { IProductDTO } from '../../../DTO/ProductDTO';
+import { Product } from '@prisma/client';
+
 export interface IProductsRepository {
-  getAllProducts(): Promise<IProductDTO[]>;
+  getAllProducts(): Promise<Product[]>;
   getProductByID(productId: number): Promise<IProductDTO>;
 }
